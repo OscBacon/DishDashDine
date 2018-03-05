79b8de4f6ef0cae2356eee66d3775d3630d50e17
Shardul Bansal <bansa11750@gmail.com>
2018-03-05 14:02:53 -0500

    Created Bill.java with necessary methods

--- phase1/src/Bill.java
+++ phase1/src/Bill.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Bill{
+    private int tableNumber;
+    private Server server;
+    private ArrayList dishList;
+    private int FinalId;
+    private static int numOfBills;
+
 
 }
