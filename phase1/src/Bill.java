62d82da9c0c2162361de6051fa9cb78060caa341
Shardul Bansal <bansa11750@gmail.com>
2018-03-05 14:03:53 -0500

    Created Bill.java with necessary methods

--- phase1/src/Bill.java
+++ phase1/src/Bill.java
@@ -1,11 +1,33 @@
 import java.util.ArrayList;
 
-public class Bill{
+public class Bill
+{
+    
     private int tableNumber;
-    private Server server;
-    private ArrayList dishList;
+    private Waiter server;
+    private ArrayList<Dish> dishList;
     private int FinalId;
     private static int numOfBills;
 
+    public Bill(int tableNumber, Waiter server) {
+        this.tableNumber = tableNumber;
+        this.server = server;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public void addDish(Dish dish) {
+        dishList.add(dish);
+    }
+
+    public void removeDish(int dishID) {
+        for(int i = 0; i < this.dishList.size(); i++) {
+            if (dishList.get(i).getID == dishID) {
+                dishList.remove(dishList[i]);
+            }
+        }
+    }
 
 }
