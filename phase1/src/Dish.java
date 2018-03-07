88c7736430e2a9aac860e6068e2b775283d4dc0a
jen <jennifer.cz.liu@gmail.com>
2018-03-07 04:38:55 -0500

    Changed Dish.additions and .subtractions to be ArrayLists instead of arrays and changed methods accordingly. Fixed misspelling of "subtraction". Imported Arraylist and removed Array import.

--- phase1/src/Dish.java
+++ phase1/src/Dish.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dish {
 
 
     private String name;
-    private String [] additions;
-    private String [] substractions;
+    private ArrayList<String> additions;
+    private ArrayList<String> subtractions;
     private int tableNumber;
     private double price;
     private int dishId;
@@ -16,10 +17,10 @@ public class Dish {
         this.name = dishName;
     }
 
-    public Dish(String name, String [] dishAdditions, String [] dishSubtractions) {
+    public Dish(String name, ArrayList<String> dishAdditions, ArrayList<String> dishSubtractions) {
         this.name = name;
         this.additions = dishAdditions;
-        this.substractions = dishSubtractions;
+        this.subtractions = dishSubtractions;
     }
 
     // Getters for all variables
@@ -27,12 +28,12 @@ public class Dish {
         return name;
     }
 
-    public String[] getAdditions() {
+    public ArrayList<String> getAdditions() {
         return additions;
     }
 
-    public String[] getSubstractions() {
-        return substractions;
+    public ArrayList<String> getSubtractions() {
+        return subtractions;
     }
 
     public int getTableNumber() {
@@ -61,7 +62,7 @@ public class Dish {
         for (String addition : this.additions) {
             fullDish += "+" + addition;
         }
-        for (String eachSubtraction: this.substractions) {
+        for (String eachSubtraction: this.subtractions) {
             fullDish += "-" + eachSubtraction;
         }
         return fullDish;
