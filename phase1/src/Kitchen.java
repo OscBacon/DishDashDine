7690dc484f05e79790ff6a9716ffc90c08872bee
Michael Katilevsky <michael.katilevsky@mail.utoronto.ca>
2018-03-07 05:56:48 -0500

    Created Kitchen class's skeleton.

--- phase1/src/Kitchen.java
+++ phase1/src/Kitchen.java
@@ -1,6 +1,52 @@
+import java.util.HashMap;
+
 public class Kitchen extends Listener {
+
+    private static HashMap<String, Dish> dishList;
+
+
+    public Kitchen()
+    {
+        dishList = new HashMap<String, Dish>();
+    }
+
+
+    public static void addDish(Dish dish)
+    {
+        String id = String.valueOf(dish.getDishId());
+
+        dishList.put(id, dish);
+    }
+
+
+    private static void removeDish(Dish dish)
+    {
+        String id = String.valueOf(dish.getDishId());
+
+        dishList.remove(id);
+    }
+
+
+    private void readyDish(int dishID)
+    {
+
+    }
+
+
+    private void cancelDish(int dishID)
+    {
+
+    }
+
+
     @Override
     public void handleEvent(String[] inputArray) {
 
+    }
+
+
+    @Override
+    public void printToScreen(String s) {
+        super.printToScreen(s);
     }
 }
