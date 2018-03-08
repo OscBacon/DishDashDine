1aaa7caa8fa191ccffffb9d9b36b1a1d3c070693
Michael Katilevsky <michael.katilevsky@mail.utoronto.ca>
2018-03-08 10:08:22 -0500

    Finished all JavaDocs in Waiter.java, and tweaked some implementations without hindering the original functionality.

--- phase1/src/Waiter.java
+++ phase1/src/Waiter.java
@@ -1,46 +1,59 @@
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Waiter implements Listener {
-    // all unpaid Bills
+    /**
+     * All unpaid, active Bills, accessible with the Bill's table number.
+      */
     private HashMap<Integer, Bill> billList;
 
-    // all dishes ever ordered
+    /**
+     * All the Bills that have ever been ordered through this waiter, accessible with the Bill id.
+     * Includes paid and unpaid bills.
+     */
+    private HashMap<Integer,Bill> allBillsList;
+
+    /**
+     * All dishes ever ordered through this waiter.
+      */
     private HashMap<Integer, Dish> dishList;
 
     /**
-     * All the Bills that have been paid, accessible with the Bill id.
+     * The name of this waiter.
      */
-    private HashMap<Integer,Bill> archivedBillList;
-
     private String name;
+
 
     public Waiter(String name) {
         this.name = name;
         this.billList = new HashMap<>();
         this.dishList = new HashMap<>();
+        this.allBillsList = new HashMap<>();
     }
 
+
+    /**
+     * Returns the name of this waiter.
+     *
+     * @return This waiter's name as a String.
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * Makes calls to the appropriate functions in this Kitchen class depending on the input array of Strings.
+     *
+     * @param inputArray The input to be handled, split into an array of Strings.
+     */
     public void handleEvent(String[] inputArray) {
 
         if (inputArray.length >= 2) // Makes sure the inputArray is not erroneous to avoid an OutOfBounds exception.
         {
             switch (inputArray[0]) {
-                case "requested bill":
-                    this.showBill(Integer.valueOf(inputArray[1]));
-                    break;
                 case "ordered": {
                     if (inputArray.length >= 6) // Makes sure the inputArray is not erroneous to avoid an OutOfBounds exception.
                     {
@@ -73,7 +86,7 @@ public class Waiter implements Listener {
                 case "pay bill":
                     this.payBill(Integer.valueOf(inputArray[1]));
                     break;
-                case "show bill":
+                case "requested bill":
                     this.showBill(Integer.valueOf(inputArray[1]));
                     break;
                 case "recall dish":
@@ -86,7 +99,17 @@ public class Waiter implements Listener {
         }
     }
 
-    // helper for .createDish, alters Ingredients based on additions and subtractions
+
+    /**
+     * Helper for .createDish(), alters Ingredients based on additions and subtractions. Also makes sure all additions
+     * and subtractions to this menu item are valid.
+     *
+     * @param menuItem The name of the menu item that has been ordered.
+     * @param ingredients The ingredients and the quantities needed to make this menu item.
+     * @param additions The ingredients that the client wished to add to the item.
+     * @param subtractions The ingredients that the client wished to remove from the item.
+     * @return True if all additions and subtractions to this menuItem are valid.
+     */
     private boolean madeSubstitutions(MenuItem menuItem, HashMap<String, Integer> ingredients,
                                                        ArrayList<String> additions, ArrayList<String> subtractions) {
         for (String addition : additions) {
@@ -117,7 +140,18 @@ public class Waiter implements Listener {
         return true;
     }
 
-    // Precondition: <item> is on the menu
+
+    /**
+     * Checks that the ordered dish is valid and that there are enough ingredients to fulfill the dish, and then places
+     * the order by adding the constructed dish to the Kitchen's dishList and updating the restaurant's inventory.
+     *
+     * Precondition: <item> is on the menu.
+     *
+     * @param item The name of the item being ordered from the menu.
+     * @param additions The ingredients the client wishes to add to the dish.
+     * @param subtractions The ingredients the client wishes to remove from the dish.
+     * @param tableNumber The number of the table that this dish is to be served to.
+     */
     private void createDish(String item, ArrayList<String> additions, ArrayList<String> subtractions, String tableNumber) {
         MenuItem menuItem = Restaurant.getMenu().get(item);
         HashMap<String, Integer> ingredients = new HashMap<>(menuItem.getIngredients());
@@ -135,7 +169,7 @@ public class Waiter implements Listener {
                     Integer quantity = ingredients.get(ingredient);
                     Restaurant.removeFromInventory(ingredient, quantity);
                 }
-                printToScreen(item + " (Dish id " + dish.getDishId() + ") was ordered for Table " + tableNumber);
+                printToScreen(item + " (Dish id: " + dish.getDishId() + ") was ordered for Table " + tableNumber);
             }
             // There are insufficient ingredients to complete the order
             else {
@@ -144,7 +178,16 @@ public class Waiter implements Listener {
         }
     }
 
-    // Precondition: <item> is on the menu
+
+    /**
+     * Checks that there are enough ingredients to fulfill the ordered dish, and then places the order by adding the
+     * constructed dish to the Kitchen's dishList and updating the restaurant's inventory.
+     *
+     * Precondition: <item> is on the menu.
+     *
+     * @param item The name of the item being ordered from the menu.
+     * @param tableNumber The number of the table that this dish is to be served to.
+     */
     private void createDish(String item, String tableNumber) {
         MenuItem menuItem = Restaurant.getMenu().get(item);
         HashMap<String, Integer> ingredients = new HashMap<>(menuItem.getIngredients());
@@ -157,62 +200,111 @@ public class Waiter implements Listener {
                 Integer quantity = ingredients.get(ingredient);
                 Restaurant.removeFromInventory(ingredient, quantity);
             }
-            printToScreen(item + " (Dish id " + dish.getDishId() + ") was ordered for Table " + tableNumber + "!");
+            printToScreen(item + " (Dish id: " + dish.getDishId() + ") was ordered for Table " + tableNumber + "!");
         } else {
             printToScreen("Can't order dish, insufficient ingredients: " + missingIngredients);
         }
     }
 
+
+    /**
+     * Creates a new bill in billList when this waiter starts serving a new table / arrival.
+     *
+     * @param tableNum The number of the table this bill is tied to.
+     */
     private void createBill(int tableNum) {
         Bill bill = new Bill(tableNum, this);
-        billList.put(tableNum, bill);
+        billList.put(tableNum, bill);                   // Add this bill to this waiter's active bills.
+        allBillsList.put(bill.getBillID(), bill);       // Add this bill to all bills ever created for this waiter.
         printToScreen("New bill created for Table " + tableNum + "!");
     }
 
+
     /**
-     * Pays Bill of the given table.
-     * This archives the table's Bill in bills.json.
+     * Pays the Bill of the given table, and removes this bill and its dishes from this waiter's lists of active
+     * dishes and bills.
+     *
      * @param tableNum Number of the table whose Bill is paid
      */
     private void payBill(int tableNum) {
-        Bill bill = billList.get(tableNum);
-        archivedBillList.put(bill.getBillID(), bill);
+        Bill bill = billList.get(tableNum); // Retrieve the bill object
+
+        printToScreen(bill.toString()); // Print the bill to the screen
+
         for (Integer key : bill.getDishList().keySet()) {
-            dishList.remove(key);
+            dishList.remove(key);   // Remove all dishes that were on this bill from this waiter's dishList.
         }
-        printToScreen(bill.toString());
+
+        billList.remove(tableNum); // Remove the bill from the active bills list
+
         printToScreen("Table " + tableNum + " has paid!");
     }
 
+
+    /**
+     * Adds the delivered dish to the table's bill.
+     *
+     * @param dishID The ID of the dish in question.
+     */
     private void confirmDishDelivery(int dishID) {
         Dish dish = dishList.get(dishID);
         Bill bill = billList.get(dish.getTableNumber());
         bill.addDish(dish);
-        printToScreen("Dish " + dish + " delivered!");
+        printToScreen("Dish " + dishID + " delivered!");
     }
 
+
+    /**
+     * Removes this dish from its table's bill.
+     *
+     * @param dishID The ID of the dish in question.
+     */
     private void removeDish(int dishID){
         Dish dish = dishList.get(dishID);
         Bill bill = billList.get(dish.getTableNumber());
         bill.removeDish(dishID);
-        printToScreen("Dish " + dish + " removed!");
+        printToScreen("Dish " + dishID + " removed!");
     }
 
+
+    /**
+     * Removes this dish from the its table's bill and puts the dish back into the system (aka reorders the dish).
+     *
+     * @param dishID The ID of the dish in question.
+     */
     private void recallDish(int dishID) {
         removeDish(dishID);
         Dish dish = dishList.get(dishID);
         createDish(dish.getName(), dish.getAdditions(), dish.getSubtractions(), String.valueOf(dish.getTableNumber()));
-        printToScreen("Dish " + dish + " recalled!");
+        printToScreen("Dish " + dishID + " recalled!");
     }
 
+
+    /**
+     * Finds the bill with billID in all of this waiter's bills and prints this bill.
+     *
+     * @param billID The ID of the bill that the waiter wishes to see.
+     */
     private void showBill(int billID) {
-        printToScreen(billList.get(billID).toString());
+        printToScreen(allBillsList.get(billID).toString());
     }
 
+
+    /**
+     * Returns a String representation of a waiter - the waiter's name.
+     *
+     * @return A String, the waiter's name.
+     */
     public String toString() {
         return this.name;
     }
 
+
+    /**
+     * Method that standardizes the way we print messages to a waiter instance.
+     *
+     * @param s String to be printed.
+     */
     public void printToScreen(String s) {
         System.out.println(name + ": " + s);
     }
