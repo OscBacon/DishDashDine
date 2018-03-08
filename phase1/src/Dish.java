76508a800e32b60ec4f7a6da0afe4242d0f9e84d
Shardul Bansal <bansa11750@gmail.com>
2018-03-08 01:33:38 -0500

    JavaDoc for Dish

--- phase1/src/Dish.java
+++ phase1/src/Dish.java
@@ -28,7 +28,7 @@ public class Dish {
         this.waiter = waiter;
         this.additions = new ArrayList<String>();
         this.subtractions = new ArrayList<String>();
-
+        this.price = Restaurant.getMenu().get(dishName).getPrice();
     }
 
     /**
@@ -43,6 +43,7 @@ public class Dish {
         this.additions = dishAdditions;
         this.subtractions = dishSubtractions;
         this.waiter = waiter;
+        this.price = Restaurant.getMenu().get(dishName).getPrice();
     }
 
     /**
@@ -60,42 +61,73 @@ public class Dish {
     }
 
     /**
-     * Returns all the additions 
-     * @return
+     * Returns all the additions of the dish
+     * @return additions
      */
     public ArrayList<String> getAdditions() {
         return additions;
     }
 
+    /**
+     * Returns all the subtractions required by the Restaurant's customer.
+     * @return subtractions
+     */
     public ArrayList<String> getSubtractions() {
         return subtractions;
     }
 
+    /**
+     * Returns the table this particular dish has been assigned
+     * @return tableNumber
+     */
     public int getTableNumber() {
         return tableNumber;
     }
 
+    /**
+     * Returns the Waiter that has assigned this Dish.
+     * @return Waiter
+     */
     public Waiter getWaiter() {
         return waiter;
     }
 
+    /**
+     * Returns the cook who accepts and prepares this particular dish
+     * @return String Name of Cook
+     */
     public String getCook() {
         return cook;
     }
 
-    // One setter, for the cook attribute
+    /**
+     * Sets the cook who will accept and prepare the dish
+     * @param cook The particular cook who will accept and prepare the dish.
+     */
     public void setCook(String cook) {
         this.cook = cook;
     }
 
+    /**
+     * Get the price of the dishName
+     * @return
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * ID of each particular dish
+     * @return int dishID
+     */
     public int getDishId() {
         return dishId;
     }
 
+    /**
+     * Each particular ingredient of the Dish
+     * @return Hashmap
+     */
     public HashMap getIngredients() {
         return ingredients;
     }
