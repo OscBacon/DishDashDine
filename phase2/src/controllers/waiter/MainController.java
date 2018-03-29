d5cdcfedb733590ce14d94df336b81f09dc67369
Shardul Bansal <bansa11750@gmail.com>
2018-03-29 01:25:18 -0400

    Setting up Controllers for FXMLs

--- phase2/src/controllers/waiter/MainController.java
+++ phase2/src/controllers/waiter/MainController.java
@@ -1,4 +1,43 @@
 package controllers.waiter;
 
+import controllers.Restaurant;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import models.Waiter;
+
+
+import javax.xml.ws.FaultAction;
+
 public class MainController {
+
+    @FXML
+    private Button selectTableBtn;
+
+    @FXML
+    private ListView<String> displayTablesList = new ListView<String>();
+
+    @FXML
+    private Button createNewOrderBtn;
+
+
+    @FXML
+    public void initialize() {
+        Waiter currWaiter = Restaurant.getListenerList().get("Waiter " + name);
+        ObservableList currentBills = FXCollections.observableArrayList(currWaiter.getBillList());
+        displayTablesList.setItems(currentBills);
+    }
+    @FXML
+    void createNewOrderForTable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void waiterSelectsTable(ActionEvent event) {
+
+    }
+
 }
