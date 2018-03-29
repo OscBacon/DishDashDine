fa3e66ee9d8bbecd362440add1f71a0803ae22d4
Oscar <oscar.baracos@gmail.com>
2018-03-28 22:54:44 -0400

    Added EmployeeSelectionController.openEmployeeView
    WIP, Waiter view is opened without passing waiter name

--- phase2/src/controllers/EmployeeSelectionController.java
+++ phase2/src/controllers/EmployeeSelectionController.java
@@ -2,9 +2,14 @@ package controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+
+import java.io.IOException;
 
 public class EmployeeSelectionController {
     @FXML
@@ -18,4 +23,32 @@ public class EmployeeSelectionController {
         ObservableList<String> items = FXCollections.observableArrayList("Manager", "Waiter", "Kitchen", "Receiver");
         employeeTypeList.setItems(items);
     }
+
+    public void openEmployeeView(ActionEvent actionEvent) throws IOException{
+        String selectedEmployeeType = employeeTypeList.getSelectionModel().getSelectedItem();;
+        switch (selectedEmployeeType) {
+            case "Manager":
+                Parent managerPage = FXMLLoader.load(Restaurant.class.getResource("../resources/views/Manager.fxml"));
+                Restaurant.stage.getScene().setRoot(managerPage);
+                Restaurant.stage.show();
+                break;
+            case "Waiter":
+                Parent waiterPage = FXMLLoader.load(Restaurant.class.getResource("../resources/views/Waiter.fxml"));
+                Restaurant.stage.getScene().setRoot(waiterPage);
+                Restaurant.stage.show();
+                break;
+            case "Kitchen":
+                Parent kitchenPage = FXMLLoader.load(Restaurant.class.getResource("../resources/views/Kitchen.fxml"));
+                Restaurant.stage.getScene().setRoot(kitchenPage);
+                Restaurant.stage.show();
+                break;
+            case "Receiver":
+                Parent receiverPage = FXMLLoader.load(Restaurant.class.getResource("../resources/views/Receiver.fxml"));
+                Restaurant.stage.getScene().setRoot(receiverPage);
+                Restaurant.stage.show();
+                break;
+
+        }
+    }
 }
+
