59cdb10e1c0010bd93bf9f1e1ef90bf35f73713f
Oscar <oscar.baracos@gmail.com>
2018-03-29 12:52:33 -0400

    Implemented Manager CreateEmploye controller and view

--- phase2/src/controllers/manager/CreateEmployeeController.java
+++ phase2/src/controllers/manager/CreateEmployeeController.java
@@ -1,42 +1,54 @@
 package controllers.manager;
 
 import controllers.Logging;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 
 public class CreateEmployeeController {
+    private Stage dialogStage;
+    public ListView<String> employeeTypeView = new ListView<>();
 
     @FXML
     private TextField newEmployeeName;
 
     @FXML
-    private MenuButton NewEmployeeTypebtn;
+    public void initialize() {
+        ArrayList<String> employeeTypeList = new ArrayList<String>();
+        employeeTypeList.add("Waiter");
+        employeeTypeList.add("Cook");
+        employeeTypeView.setItems(FXCollections.observableArrayList(employeeTypeList));
+    }
 
     @FXML
-    private Button createEmployeebtn;
-
-    @FXML
-    void createEmployee(ActionEvent event) {
+    void createEmployee() {
         String employeeName = newEmployeeName.getText();
-        String selectedEmployeeType = NewEmployeeTypebtn.getTypeSelector();
-        switch (selectedEmployeeType) {
-            case "Waiter":
-                Logging.addWaiter(employeeName);
-                System.out.println("You created a Waiter called " +employeeName);
-                break;
-            case "Cook":
-                Logging.addCook(employeeName);
-                System.out.println("You created a Cook called " +employeeName);
-                break;
+        String selectedEmployeeType = employeeTypeView.getFocusModel().getFocusedItem();
+        System.out.println(selectedEmployeeType);
+        if (employeeName != null && selectedEmployeeType != null) {
+            switch (selectedEmployeeType) {
+                case "Waiter":
+                    Logging.addWaiter(employeeName);
+                    System.out.println("You created a Waiter called " + employeeName);
+                    break;
+                case "Cook":
+                    Logging.addCook(employeeName);
+                    System.out.println("You created a Cook called " + employeeName);
+                    break;
+            }
+            dialogStage.close();
         }
     }
 
-
     public void setDialogStage(Stage stage) {
+        dialogStage = stage;
     }
 }
