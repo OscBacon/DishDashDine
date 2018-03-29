fbd98269c1283f5069e9befbdd3e809f6fbc25c6
Shardul Bansal <bansa11750@gmail.com>
2018-03-29 08:14:56 -0400

    FXML and added getter in Bill

--- phase2/src/controllers/waiter/TableDetailsController.java
+++ phase2/src/controllers/waiter/TableDetailsController.java
@@ -1,5 +1,6 @@
 package controllers.waiter;
 
+import controllers.Logging;
 import controllers.Restaurant;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -7,20 +8,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import models.Bill;
 import models.Dish;
+import models.MenuItem;
+import models.Waiter;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class TableDetailsController {
+
+    Bill currentBill;
+    String tableNumber;
 
     @FXML
     private ListView<String> menuList;
 
     @FXML
-    private Button dishAddition;
+    private ListView<String> dishAddition;
 
     @FXML
-    private Button dishSubtraction;
+    private ListView<String> dishSubtraction;
 
     @FXML
     private Button dish;
@@ -36,27 +45,45 @@ public class TableDetailsController {
         ObservableList<String> menuItem = FXCollections.observableArrayList(menuItemName);
         menuList.setItems(menuItem);
 
-        
+
     }
 
     @FXML
     void addDishToBill(ActionEvent event) {
-
+        String dishName = menuList.getSelectionModel().getSelectedItem();
+        if (dishName != null) {
+            if ((dishAddition.getSelectionModel().getSelectedItems() == null) && (dishSubtraction.getSelectionModel().getSelectedItems() == null)) {
+                Logging.orderDish(currentBill.getWaiter().getName(), dishName, tableNumber);
+            } else if ((dishAddition.getItems() != null) || (dishSubtraction.getItems() != null)){
+                ArrayList dishadd = (ArrayList) dishAddition.getSelectionModel().getSelectedItems();
+                ArrayList dishsubtract = (ArrayList) dishSubtraction.getSelectionModel().getSelectedItems();
+                Logging.orderDish(currentBill.getWaiter().getName(), dishName, dishadd.toString(), dishsubtract.toString(),tableNumber);
+            }
+        }
     }
 
-    @FXML
-    void showAllowedAdditions(ActionEvent event) {
+        @FXML
+        void showAllowedAdditions (ActionEvent event){
+            dishAddition.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            String dishName = menuList.getSelectionModel().getSelectedItem();
+            MenuItem dishItem = Restaurant.getMenu().get(dishName);
+            ObservableList allowedAdditions = FXCollections.observableArrayList(dishItem.getAllowedAdditions());
+            dishAddition.setItems(allowedAdditions);
+        }
 
-    }
+        @FXML
+        void showAllowedSubtraction (ActionEvent event){
+            dishSubtraction.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            String dishName = menuList.getSelectionModel().getSelectedItem();
+            MenuItem dishItem = Restaurant.getMenu().get(dishName);
+            ObservableList allowedSubtractions = FXCollections.observableArrayList(dishItem.getAllowedSubtractions());
+            dishSubtraction.setItems(allowedSubtractions);
+        }
 
-    @FXML
-    void showAllowedSubtraction(ActionEvent event) {
+        @FXML
+        void showCurrentBill (ActionEvent event){
 
-    }
-
-    @FXML
-    void showCurrentBill(ActionEvent event) {
-
-    }
+        }
 
 }
+
