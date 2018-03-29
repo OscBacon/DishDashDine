8a33c4eddf8a100105caf5848e912abed08a77cf
Shardul Bansal <bansa11750@gmail.com>
2018-03-29 02:15:00 -0400

    FXML files

--- phase2/src/controllers/kitchen/MainController.java
+++ phase2/src/controllers/kitchen/MainController.java
@@ -4,13 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
     @FXML
-    private ChoiceBox<?> NextDishtoAccept;
+    private ChoiceBox<?> AllCooks;
+
+    @FXML
+    private Label nextDishToAccept;
 
     @FXML
     private Button CookAcceptsDishbtn;
@@ -19,12 +23,18 @@ public class MainController {
     private TableView<?> AcceptedDishesTable;
 
     @FXML
+    void initialize() {
+    
+    }
+
+    @FXML
     void cookAcceptsCurrentDish(ActionEvent event) {
 
     }
 
     @FXML
     void showAllCooks(MouseEvent event) {
+
 
     }
 
