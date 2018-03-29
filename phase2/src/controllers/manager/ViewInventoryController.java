9f56c219611dbd0e9b734e03e5daffa0d77fdca0
Oscar <oscar.baracos@gmail.com>
2018-03-29 13:48:56 -0400

    Implemented manager ViewInventory controller and view

--- phase2/src/controllers/manager/ViewInventoryController.java
+++ phase2/src/controllers/manager/ViewInventoryController.java
@@ -1,4 +1,45 @@
 package controllers.manager;
 
+import controllers.Restaurant;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import models.InventoryItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ViewInventoryController {
+    private Stage dialogStage;
+
+    public TableView<String> inventoryTable = new TableView<>();
+    public TableColumn<String, String> ingredientColumn;
+    public TableColumn<String, Integer> quantityColumn;
+    private HashMap<String, Integer> inventoryQuantityHashmap = new HashMap<>();
+    ObservableMap<String, Integer> observableMap = FXCollections.observableHashMap();
+    ObservableList<String> observableArray = FXCollections.observableArrayList();
+
+    public void initialize() {
+        HashMap<String, InventoryItem> inventory = Restaurant.getInventory();
+
+        ArrayList<String> itemArrayList = new ArrayList<>(inventory.keySet());
+
+        inventoryTable.setItems(FXCollections.observableList(itemArrayList));
+
+        ingredientColumn.setCellValueFactory(cd -> new SimpleStringProperty(cd.getValue()));
+        quantityColumn.setCellValueFactory(cd -> new SimpleIntegerProperty(inventory.get(cd.getValue())
+                .getQuantity()).asObject());
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
 }
