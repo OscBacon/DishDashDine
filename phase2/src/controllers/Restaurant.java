123b1bf06bfd07a7c3f60fa3f4d3767c3997eae9
Oscar <oscar.baracos@gmail.com>
2018-03-29 13:41:01 -0400

    Changed InventoryItem to have a name attribute

--- phase2/src/controllers/Restaurant.java
+++ phase2/src/controllers/Restaurant.java
@@ -136,6 +136,11 @@ public class Restaurant extends Application {
         if (inventory == null) {
             inventory = new HashMap<>();
         }
+        else {
+            for (String key: inventory.keySet()) {
+                inventory.get(key).setName(key);
+            }
+        }
         // Checks inventory on launch to send out reorder requests ASAP
         checkInventory();
 
@@ -239,7 +244,7 @@ public class Restaurant extends Application {
 
             inventory.get(ingredient).setQuantity(currQuantity + quantity);
         } else {
-            inventory.put(ingredient, new InventoryItem(quantity));
+            inventory.put(ingredient, new InventoryItem(ingredient, quantity));
         }
 
         inventoryModified = true;
