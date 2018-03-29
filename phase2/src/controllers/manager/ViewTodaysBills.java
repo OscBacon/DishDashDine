3d988c842a3c22d871e79c22597256ed107a3291
Shardul Bansal <bansa11750@gmail.com>
2018-03-29 15:31:08 -0400

    Manage FXMLs

---
+++ phase2/src/controllers/manager/ViewTodaysBills.java
@@ -0,0 +1,11 @@
+package controllers.manager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ViewTodaysBills {
+
+    @FXML
+    private ListView<?> todayBills;
+
+}
---
+++ phase2/src/controllers/manager/ViewUndeliveredDishes.java
@@ -0,0 +1,11 @@
+package controllers.manager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ViewUndeliveredDishes {
+
+    @FXML
+    private ListView<?> unDeliveredDishes;
+
+}
