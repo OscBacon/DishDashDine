359047bee6689d242888381d2e77c9efd197aa4a
Oscar <oscar.baracos@gmail.com>
2018-03-28 21:52:48 -0400

    Added attributes for components of EmployeeSelectionController.java, modified appearance of EmployeeSelection.fxml

--- phase2/src/controllers/EmployeeSelectionController.java
+++ phase2/src/controllers/EmployeeSelectionController.java
@@ -1,4 +1,21 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class EmployeeSelectionController {
+    @FXML
+    public Button loginButton;
+
+    @FXML
+    private ListView<String> employeeTypeList = new ListView<String>();
+
+    @FXML
+    public void initialize() {
+        ObservableList<String> items = FXCollections.observableArrayList("Manager", "Waiter", "Kitchen", "Receiver");
+        employeeTypeList.setItems(items);
+    }
 }
