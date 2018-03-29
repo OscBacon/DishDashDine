773f5a2cc59cbb9cd96e624fac17c1b47cffa824
Shardul Bansal <bansa11750@gmail.com>
2018-03-29 04:17:17 -0400

    Add FXML Files

--- phase2/src/controllers/waiter/NewTableOrder.java
+++ phase2/src/controllers/waiter/NewTableOrder.java
@@ -1,4 +1,21 @@
 package controllers.waiter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
 
 public class NewTableOrder {
+
+        @FXML
+        private TextField TableNumberInput;
+
+        @FXML
+        private Button CreateNewBill;
+
+        @FXML
+        void CreateNewBillForTable(ActionEvent event) {
+            
+        }
 }
+
